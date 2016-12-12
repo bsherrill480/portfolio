4b5a631facbaaf0867f644c0622d2ba939212823
brian <bdsherrill9@gmail.com>
2016-12-12 14:09:10 -0800

    Saving

--- server/config/staging_config.js
+++ server/config/staging_config.js
@@ -1,5 +1,5 @@
 const stagingConfig = {
-    connectionString: 'mongodb://admin:test1234@ds033046.mlab.com:33046/sherrill-brian-webdev-final',
+    connectionString: 'mongodb://admin:test1234@ds033046.mlab.com:33046/sherrill-brian-webdev',
     googleClient: '176316419365-kptpt2gp57tldfu27h2b97o872bnap44.apps.googleusercontent.com',
     googleSecret: 'Xr_5ypwF-dNbcdtEJNQWNl8e'
 };
