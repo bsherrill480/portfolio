5bd6f7d1854502e64851a1182a59e2b138756e61
brian <bdsherrill9@gmail.com>
2017-01-07 06:39:50 -0800

    Adding prod log

--- server/config/prod_config.js
+++ server/config/prod_config.js
@@ -1,11 +1,27 @@
 const env = process.env,
     MLAB_USER_NAME = env.MLAB_USER_NAME,
     MLAB_PASSWORD = env.MLAB_PASSWORD,
-    devConfig = {
+    GOOGLE_CLIENT_ID = env.GOOGLE_CLIENT_ID,
+    GOOGLE_SECRET = env.GOOGLE_SECRET,
+    GOOGLE_AUTH_CALLBACK_URL = env.GOOGLE_AUTH_CALLBACK_URL,
+    prodConfig = {
     connectionString: `mongodb://${MLAB_USER_NAME}:${MLAB_PASSWORD}@ds157268.mlab.com:57268/planner-prod`,
-    googleClient: env.GOOGLE_CLIENT_ID,
-    googleSecret: env.GOOGLE_SECRET,
-    googleCallbackUrl: env.GOOGLE_AUTH_CALLBACK_URL,
+    googleClient: GOOGLE_CLIENT_ID,
+    googleSecret: GOOGLE_SECRET,
+    googleCallbackUrl: GOOGLE_AUTH_CALLBACK_URL,
     facebookCallbackUrl: ''
 };
-module.exports = devConfig;
+
+if(!MLAB_PASSWORD || !MLAB_PASSWORD || !GOOGLE_CLIENT_ID || !GOOGLE_SECRET || 
+    !GOOGLE_AUTH_CALLBACK_URL) {
+    console.log('=========');
+    console.log('ENV_VAR MISSING');
+    console.log(`MLAB_PASSWORD: ${MLAB_PASSWORD} 
+    MLAB_PASSWORD: ${MLAB_PASSWORD} 
+    GOOGLE_CLIENT_ID: ${GOOGLE_CLIENT_ID} 
+    GOOGLE_SECRET: ${GOOGLE_SECRET}
+    GOOGLE_AUTH_CALLBACK_URL: ${GOOGLE_AUTH_CALLBACK_URL}`);
+    console.log('=========');
+}
+
+module.exports = prodConfig;
