d39d396f1f04a887b69a901b62d6eea2e792df1c
brian <bdsherrill9@gmail.com>
2017-01-08 06:53:50 -0800

    Saving

--- server/mailer/index.js
+++ server/mailer/index.js
@@ -1,1 +1,21 @@
+const
+    // envs = require('../config/envs'),
+    // env = process.env.NODE_ENV,
+    nodemailer = require('nodemailer'),
+    sesTransport = require('nodemailer-ses-transport'),
+    AWS = require('aws-sdk'),
+    ses = new AWS.SES(),
+    transporter = nodemailer.createTransport(sesTransport({
+        ses: ses
+    }));
 
+module.exports = {
+    sendMail: function (mailOptions) {
+        transporter.sendMail(mailOptions, function (err, info) {
+            if(err) {
+                console.log('sendMail err:', err);
+            }
+            console.log('messageSent:' + info.response);
+        });
+    }
+};
