3cad36ada88d6cd67d177f78cc52acac6830f7ed
brian <bdsherrill9@gmail.com>
2017-01-13 02:58:17 -0800

    saving

--- server/routes/api/aws/index.js
+++ server/routes/api/aws/index.js
@@ -2,26 +2,29 @@
 
 // https://github.com/mattrobenolt/node-snsclient
 var https = require('https'),
+    express = require('express'),
+    router = express.Router(),
     MessageValidator = require('sns-validator'),
     validator = new MessageValidator();
 
 
 router.post('/receive', function (req, res, next) {
     const message = req.body;
+    res;
     console.log('sns req:', req);
     console.log('sns message', message);
     validator.validate(message, function (err, message) {
-    if (err) {
-        console.error('sns validate err', err);
-        return;
-    }
+        if (err) {
+            console.error('sns validate err', err);
+            return;
+        }
 
-    if (message['Type'] === 'SubscriptionConfirmation') {
-        https.get(message['SubscribeURL'], function (res) {
-          // You have confirmed your endpoint subscription
-        });
-    }
-});
+        if (message['Type'] === 'SubscriptionConfirmation') {
+            https.get(message['SubscribeURL'], function () {
+                // You have confirmed your endpoint subscription
+            });
+        }
+    });
 });
 
 module.exports = router;
